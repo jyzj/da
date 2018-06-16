feda33dae0d80d71011f9d88b510dbdb127bb164
shq4 <pdh666@gmail.com>
2018-06-16 22:36:39 +0800

    WAccounts_Add 窗口输入校验完成

--- da/Wins/WAccounts_Add.cs
+++ da/Wins/WAccounts_Add.cs
@@ -22,6 +22,111 @@ namespace da.Wins
          */
         private void bConfirm_Click(object sender, EventArgs e)
         {
+            //获取参数
+            //获取账户名
+            string name = this.tbName.Text;
+            if (name == null || name.Trim() == "")
+            {
+                MessageBox.Show("账户名不能为空", "警告", MessageBoxButtons.OK);
+                return;
+            }
+            name = name.Trim();
+            //获取密码
+            string pwd = this.tbPwd.Text;
+            if (pwd == null || pwd.Trim() == "")
+            {
+                MessageBox.Show("密码不能为空", "警告", MessageBoxButtons.OK);
+                return;
+            }
+            pwd = pwd.Trim();
+            //获取财产安全口令
+            string safePwd = this.tbSafePwd.Text;
+            if ( safePwd == null || safePwd.Trim() == "")
+            {
+                MessageBox.Show("财产安全口令不能为空", "警告", MessageBoxButtons.OK);
+                return;
+            }
+            safePwd = safePwd.Trim();
+            //获取密保卡序列号
+            string pgId = this.tbPgId.Text;
+            if (pgId == null || pgId.Trim() == "")
+            {
+                MessageBox.Show("密保卡序列号不能为空", "警告", MessageBoxButtons.OK);
+                return;
+            }
+            pgId = pgId.Trim();
+            //获取密保卡
+            int[,] pgNums = new int[10, 7];
+            for (int x = 1; x <= 10; x++)
+            {
+                for (int y = 1; y <= 7; y++)
+                {
+                    char cY = (char)(y - 1 + 97);
+                    string tb = "tbPgNums_" + cY + x;
+                    if (this.Controls[tb].Text == null || this.Controls[tb].Text.Trim() == "")
+                    {
+                        MessageBox.Show("密保卡（" + cY + "," + x + "）不能为空", "警告", MessageBoxButtons.OK);
+                        return;
+                    }
+                    if(!int.TryParse(this.Controls[tb].Text.Trim(), out pgNums[x, y]))
+                    {
+                        MessageBox.Show("密保卡（" + cY + "," + x + "）不是数字", "警告", MessageBoxButtons.OK);
+                        return;
+                    }
+                    
+                }
+            }
+            return;
+
+            //获取真实姓名
+            string realName = this.tbRealName.Text;
+            if (realName == null || realName.Trim() == "")
+            {
+                MessageBox.Show("真实姓名不能为空", "警告", MessageBoxButtons.OK);
+                return;
+            }
+            realName = realName.Trim();
+            //获取身份证号
+            string sfz = this.tbSfz.Text;
+            if (sfz == null || sfz.Trim() == "")
+            {
+                MessageBox.Show("身份证不能为空", "警告", MessageBoxButtons.OK);
+                return;
+            }
+            sfz = sfz.Trim();
+            //获取手机号码
+            string tel = this.tbTel.Text;
+            if (tel == null || tel.Trim() == "")
+            {
+                MessageBox.Show("手机号码不能为空", "警告", MessageBoxButtons.OK);
+                return;
+            }
+            tel = tel.Trim();
+            //获取邮箱
+            string email = this.tbEmail.Text;
+            if (email == null || email.Trim() == "")
+            {
+                MessageBox.Show("邮箱不能为空", "警告", MessageBoxButtons.OK);
+                return;
+            }
+            email = email.Trim();
+            //获取安全问题
+            string question = this.tbQuestion.Text;
+            if (question == null || question.Trim() == "")
+            {
+                MessageBox.Show("安全问题不能为空", "警告", MessageBoxButtons.OK);
+                return;
+            }
+            question = question.Trim();
+            //获取问题答案
+            string answer = this.tbAnswer.Text;
+            if (answer == null || answer.Trim() == "")
+            {
+                MessageBox.Show("问题答案不能为空", "警告", MessageBoxButtons.OK);
+                return;
+            }
+            answer = answer.Trim();
+
             //关闭窗口
             this.Close();
             this.Dispose();
